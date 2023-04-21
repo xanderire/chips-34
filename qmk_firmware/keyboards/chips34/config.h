5ac31c9855ad7fd13940b5a06465aa661e5170dc
XanderIRE <alexmrireland@gmail.com>
2023-04-21 14:20:13 +0100

    fix reversed right hand mcu pin layout

--- qmk_firmware/keyboards/chips34/config.h
+++ qmk_firmware/keyboards/chips34/config.h
@@ -15,11 +15,11 @@
 #define DIRECT_PINS {{D2, D0, F6, D7, B3}, \
                      {D1, F5, C6, B1, B4}, \
                      {F4, D4, F7, E6, B2}, \
-                     {NO_PIN, NO_PIN, NO_PIN, B6, B5}}
-#define DIRECT_PINS_RIGHT {{B3, D7, F6, D0, D2}, \
-                           {B4, B1, C6, F5, D1}, \
-                           {B2, E6, F7, D4, F4}, \
-                           {B5, B6, NO_PIN, NO_PIN, NO_PIN}}
+                     {NO_PIN, NO_PIN, NO_PIN, B6, B5}} 
+#define DIRECT_PINS_RIGHT {{D2, D0, F6, D7, B3}, \
+                     {D1, F5, C6, B1, B4}, \
+                     {F4, D4, F7, E6, B2}, \
+                     {NO_PIN, NO_PIN, NO_PIN, B6, B5}} 
 
 /* Mod-tap / Layer-tap Useability Settings */
 #define TAPPING_TERM 225
